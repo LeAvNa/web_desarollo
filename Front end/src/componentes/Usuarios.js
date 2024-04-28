b3f4284df1847cb952836e0ce5d0ecf41fb1189b
LeAvNa <130793158+LeAvNa@users.noreply.github.com>
2024-04-28 13:24:26 -0700

    se intento, y eso es lo que vale

--- Front end/src/componentes/Usuarios.js
+++ Front end/src/componentes/Usuarios.js
@@ -1,142 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector} from 'react-redux';
 import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
-import axios from "axios";
+import { getUsers } from '../redux/actions/actionUsers';
 
 export default function Usuarios() {
 
-  const [rowData, setRowData] = useState([]);
-  useEffect(() => {
-    axios.get('http://187.189.158.186:7777/Usuario')
-      .then(response => {
-        setRowData(response.data);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }, []);
+  const dispatch = useDispatch();
+  const { usuarios } = useSelector((state) => state.getUsers);
 
-
-  // // function click() {
-  // //   axios.get('https://jsonplaceholder.typicode.com/posts')
-  // //     .then(response => {
-  // //       response = (response.data)
-  // //       console.log(response)
-  // //       setRows(response)
-  // //       console.log(rows)
-  // //     })
-  // //     .catch(error => {
-  // //       console.error('Error fetching data: ', error);
-  // //     }); 
-
-  // // }
-
-  // Row Data: The data to be displayed.
-  // const [rowData, setRowData] = useState([
-  //   {
-  //     "id": 1,
-  //     "Nombre": "Juan",
-  //     "PrimerApellido": "González",
-  //     "SegundoApellido": "Pérez",
-  //     "Correo": "juan.gonzalez@example.com",
-  //     "Telefono": "123456789",
-  //     "NombredeUsuario": "juang"
-  //   },
-  //   {
-  //     "id": 2,
-  //     "Nombre": "María",
-  //     "PrimerApellido": "López",
-  //     "SegundoApellido": "García",
-  //     "Correo": "maria.lopez@example.com",
-  //     "Telefono": "987654321",
-  //     "NombredeUsuario": "marial"
-  //   },
-  //   {
-  //     "id": 3,
-  //     "Nombre": "Carlos",
-  //     "PrimerApellido": "Martínez",
-  //     "SegundoApellido": "Rodríguez",
-  //     "Correo": "carlos.martinez@example.com",
-  //     "Telefono": "456789123",
-  //     "NombredeUsuario": "carlosm"
-  //   },
-  //   {
-  //     "id": 4,
-  //     "Nombre": "Ana",
-  //     "PrimerApellido": "Fernández",
-  //     "SegundoApellido": "López",
-  //     "Correo": "ana.fernandez@example.com",
-  //     "Telefono": "741852963",
-  //     "NombredeUsuario": "anaf"
-  //   },
-  //   {
-  //     "id": 5,
-  //     "Nombre": "Pedro",
-  //     "PrimerApellido": "Sánchez",
-  //     "SegundoApellido": "Martínez",
-  //     "Correo": "pedro.sanchez@example.com",
-  //     "Telefono": "369258147",
-  //     "NombredeUsuario": "pedros"
-  //   },
-  //   {
-  //     "id": 6,
-  //     "Nombre": "Laura",
-  //     "PrimerApellido": "Díaz",
-  //     "SegundoApellido": "Fernández",
-  //     "Correo": "laura.diaz@example.com",
-  //     "Telefono": "852147963",
-  //     "NombredeUsuario": "laurad"
-  //   },
-  //   {
-  //     "id": 7,
-  //     "Nombre": "Javier",
-  //     "PrimerApellido": "Pérez",
-  //     "SegundoApellido": "Sánchez",
-  //     "Correo": "javier.perez@example.com",
-  //     "Telefono": "951753852",
-  //     "NombredeUsuario": "javierp"
-  //   },
-  //   {
-  //     "id": 8,
-  //     "Nombre": "Sara",
-  //     "PrimerApellido": "García",
-  //     "SegundoApellido": "Martínez",
-  //     "Correo": "sara.garcia@example.com",
-  //     "Telefono": "753951852",
-  //     "NombredeUsuario": "sarag"
-  //   },
-  //   {
-  //     "id": 9,
-  //     "Nombre": "Miguel",
-  //     "PrimerApellido": "Hernández",
-  //     "SegundoApellido": "Sánchez",
-  //     "Correo": "miguel.hernandez@example.com",
-  //     "Telefono": "159753852",
-  //     "NombredeUsuario": "miguelh"
-  //   },
-  //   {
-  //     "id": 10,
-  //     "Nombre": "Lucía",
-  //     "PrimerApellido": "Martín",
-  //     "SegundoApellido": "Pérez",
-  //     "Correo": "lucia.martin@example.com",
-  //     "Telefono": "852963741",
-  //     "NombredeUsuario": "luciam"
-  //   },
-  //   {
-  //     "id": 11,
-  //     "Nombre": "Daniel",
-  //     "PrimerApellido": "Jiménez",
-  //     "SegundoApellido": "Gómez",
-  //     "Correo": "daniel.jimenez@example.com",
-  //     "Telefono": "369147258",
-  //     "NombredeUsuario": "danield"
-  //   }
-  // ]);
-
-
+  useEffect(()=> {
+    dispatch(getUsers());
+  }, [dispatch]);
 
   // Column Definitions: Defines the columns to be displayed.
   const [colDefs, setColDefs] = useState([
@@ -154,7 +31,6 @@ export default function Usuarios() {
 
   // ...
 
-
   return (
     // wrapping container with theme & size
     <div
@@ -162,7 +38,7 @@ export default function Usuarios() {
       style={{ height: 500 }} // the grid will fill the size of the parent container
     >
       <AgGridReact
-        rowData={rowData}
+        rowData={usuarios}
         columnDefs={colDefs}
         rowSelection={'single'}
       />
--- Front end/src/redux/actions/actionUsers.js
+++ Front end/src/redux/actions/actionUsers.js
@@ -1,10 +1,22 @@
 import { createAsyncThunk, isRejectedWithValue } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const getUsers = createAsyncThunk("../../componentes/Usuario.json",
+// export const getUsers = createAsyncThunk("http://187.189.158.186:7777/Usuario",
+// async({rejectWithValue}) => {
+//     try{
+//         // const resp = await axios.get('http://187.189.158.186:7777/Usuario')
+//         return resp.data
+//     }catch(error)
+//     {
+//         return rejectWithValue('Error:,${error.message}');
+//     }
+// }
+// );
+
+export const getUsers = createAsyncThunk("users/getUsers",
 async({rejectWithValue}) => {
     try{
-        const resp = await axios.get('../../componentes/Usuario.json')
+        const resp = await axios.get('http://187.189.158.186:7777/Usuario');
         return resp.data
     }catch(error)
     {
@@ -12,3 +24,16 @@ async({rejectWithValue}) => {
     }
 }
 );
+
+export const getUserUnique = createAsyncThunk("users/getUserUnique",
+async({rejectWithValue}) => {
+    try{
+        const resp = await axios.get('http://187.189.158.186:7777/Usuario/');
+        return resp.data
+    }catch(error)
+    {
+        return rejectWithValue('Error:,${error.message}');
+    }
+}
+);
+
