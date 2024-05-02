f3a14f106e152689ab307e3de15553a9d2a88c25
LeAvNa <130793158+LeAvNa@users.noreply.github.com>
2024-05-01 17:26:50 -0700

    Se añadio la seleccion de usuario

--- Front end/src/componentes/Usuarios.js
+++ Front end/src/componentes/Usuarios.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector} from 'react-redux';
+import { useEffect, useState, useRef, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
@@ -11,9 +11,16 @@ export default function Usuarios() {
   const dispatch = useDispatch();
   // const { users } = useSelector((state) => state.getUsers);
   const { users } = useSelector((state) => state.getUsers);
-  
 
-  useEffect(()=> {
+  const gridRef = useRef();
+  const onSelectionChanged = useCallback(() => {
+    const selectedRows = gridRef.current.api.getSelectedRows();
+    document.querySelector("#selectedRows").innerHTML =
+      selectedRows.length === 1 ? selectedRows[0].idUsuario : "";
+      console.log(selectedRows)
+  }, []);
+
+  useEffect(() => {
     dispatch(getUsers());
     console.log(users);
   }, [dispatch]);
@@ -29,24 +36,34 @@ export default function Usuarios() {
     { field: 'contraseña', headerName: 'Contraseña' },
     { field: 'habilitado', headerName: 'Habilitado' },
     { field: 'nombreCompleto', headerName: 'Nombre Completo' },
-    { field: 'strFechaNacimiento', headerName: 'strFechaNacimiento' },
+    { field: 'strFechaNacimiento', headerName: 'Fecha de Nacimiento' },
   ]);
 
   // ...
 
   const getRowId = params => params.data.id;
+  function GetRowNode() {
+
+  }
 
   return (
+
     // wrapping container with theme & size
     <div
       className="ag-theme-quartz" // applying the grid theme
       style={{ height: 500 }} // the grid will fill the size of the parent container
     >
+      <div>
+        Selection:
+        <span id="selectedRows"></span>
+      </div>
       <AgGridReact
+        ref={gridRef}
         rowData={users}
         columnDefs={colDefs}
-        rowSelection={'single'}
+        rowSelection={"single"}
         getRowId={getRowId}
+        onSelectionChanged={onSelectionChanged}
       />
     </div>
   )
