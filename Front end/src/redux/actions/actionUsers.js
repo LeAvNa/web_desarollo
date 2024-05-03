e6f969b8416ab5718372622492811db838149c2f
LeAvNa <130793158+LeAvNa@users.noreply.github.com>
2024-05-03 08:49:14 -0600

    si

--- Front end/src/redux/actions/actionUsers.js
+++ Front end/src/redux/actions/actionUsers.js
@@ -13,51 +13,63 @@ import axios from "axios";
 // }
 // );
 
+
 export const getUsers = createAsyncThunk("users/getUsers",
-async() => {
-    try{
-        const resp = await axios.get('http://187.189.158.186:7777/Usuario');
-        console.log(resp);
-        return resp.data;
-    }catch(error)
-    {
-        return null;
-    }
-}
-);
+    async () => {
+        try
+        {      
+            const resp = await axios.get('http://187.189.158.186:7777/Usuario');
 
-export const getUserUnique = createAsyncThunk("users/getUserUnique",
-async(id, {rejectWithValue}) => {
-    try{
-        const resp = await axios.get('http://187.189.158.186:7777/Usuario/'+id);
-        return resp.data
-    }catch(error)
-    {
-        return rejectWithValue('Error:,${error.message}');
-    }
-}
-);
-
-export const nuevoUsuario = createAsyncThunk("users/nuevoUsuario",
-    async (Usuario) => {
-        try {
-            const respuesta = await axios.post('http://187.189.158.186:7777/Usuario', Usuario);
-            return respuesta.data;
-        } catch (error) {
-            console.error("Error:", error);
+            return resp.data;
+        } 
+        catch (error) 
+        {
             return null;
         }
     }
 );
 
-export const matarUsuario = createAsyncThunk("users/matarUsuario",
+export const getUserUnique = createAsyncThunk("users/getUserUnique",
     async (id, {rejectWithValue}) => {
-        try {
-            const respuesta = await axios.delete('http://187.189.158.186:7777/Usuario/'+ id);
-            return respuesta.data;
-        } catch (error) {
-            console.error("Error:", error);
-            return null;
+        try
+        {      
+            const resp = await axios.get('http://187.189.158.186:7777/Usuario/'+id);
+
+            return resp.data;
+        } 
+        catch (error) 
+        {
+            return rejectWithValue(`Error: ${error.message}`);
+        }
+    }
+);
+
+export const addUser = createAsyncThunk("users/addUser",
+    async (data, {rejectWithValue}) => {
+        try
+        {      
+            const resp = await axios.post('http://187.189.158.186:7777/Usuario', data);
+
+            return resp.data;
+        } 
+        catch (error) 
+        {
+            return rejectWithValue(`Error: ${error.message}`);
+        }
+    }
+);
+
+export const deleteUser = createAsyncThunk("users/deleteUser",
+    async (id, {rejectWithValue}) => {
+        try
+        {      
+            const resp = await axios.delete('http://187.189.158.186:7777/Usuario/'+id);
+
+            return resp.data;
+        } 
+        catch (error) 
+        {
+            return rejectWithValue(`Error: ${error.message}`);
         }
     }
 );
